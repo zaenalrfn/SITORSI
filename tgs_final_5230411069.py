4b04c6a137ce693f8c43730f2d2ccfc73e9cf0b3
zaenalrfn <ZAOfficial24@gmail.com>
2024-05-22 23:06:48 +0700

    menambahkan fitur restok barang

--- tgs_final_5230411069.py
+++ tgs_final_5230411069.py
@@ -46,11 +46,23 @@ class BinarySearchTree:
                 else:
                     return False
 
-    def print_bst(self, node, level=0):
+    def collect_data(self, node, data=[]):
         if node is not None:
-            self.print_bst(node.left, level + 1)
-            put_text(" " * 4 * level + "->", node.barang.sku)
-            self.print_bst(node.right, level + 1)
+            self.collect_data(node.left, data)
+            data.append(
+                [
+                    node.barang.sku,
+                    node.barang.nama_barang,
+                    node.barang.harga_satuan,
+                    node.barang.jumlah_stok,
+                ]
+            )
+            self.collect_data(node.right, data)
+        return data
+
+    def print_bst(self):
+        data = self.collect_data(self.root)
+        return data
 
     # untuk mengecek apakah no sku sudah ada di bst?
     def contains(self, sku):
@@ -63,6 +75,17 @@ class BinarySearchTree:
             else:
                 return True
         return False
+
+    def find_node(self, sku):
+        temp = self.root
+        while temp is not None:
+            if sku < temp.barang.sku:
+                temp = temp.left
+            elif sku > temp.barang.sku:
+                temp = temp.right
+            else:
+                return temp.barang
+        return None
 
 
 # objek bst
@@ -82,29 +105,76 @@ def stok_barang():
             ],
         )
         if sbmn_stok_barang == "1. Input Data Stok Barang":
-            pinput_stok_barang = popup_input(
-                [
-                    put_input("sku", label="Masukkan sku"),
-                    put_input("nama_barang", label="Masukkan nama barang"),
-                    put_input("harga_satuan", label="Masukkan harga satuan"),
-                    put_input("jumlah_stok", label="Masukkan jumlah stok"),
-                ],
-                title="input data stok barang",
-            )
-            if bst.contains(pinput_stok_barang["sku"]):
-                clear()
-                put_text("sku tersebut sudah ada")
-            else:
-                barang = Barang(
-                    pinput_stok_barang["sku"],
-                    pinput_stok_barang["nama_barang"],
-                    pinput_stok_barang["harga_satuan"],
-                    pinput_stok_barang["jumlah_stok"],
+            with use_scope("message", clear=True):
+                pinput_stok_barang = popup_input(
+                    [
+                        put_input("sku", label="Masukkan No.sku", type=NUMBER),
+                        put_input(
+                            "nama_barang", label="Masukkan nama barang", type=TEXT
+                        ),
+                        put_input(
+                            "harga_satuan", label="Masukkan harga satuan", type=NUMBER
+                        ),
+                        put_input(
+                            "jumlah_stok", label="Masukkan jumlah stok", type=NUMBER
+                        ),
+                    ],
+                    title="input data stok barang",
                 )
-                if bst.insert(barang):
-                    clear()
-                    put_text("Barang berhasil ditambahkan.")
-                    bst.print_bst(bst.root)
+
+                if pinput_stok_barang["sku"] < 1000 or pinput_stok_barang["sku"] > 9999:
+                    put_info("No.sku tidak valid. Harap masukkan nomor 4 digit")
+                else:
+                    if bst.contains(pinput_stok_barang["sku"]):
+                        put_text("sku tersebut sudah ada")
+                    else:
+                        barang = Barang(
+                            pinput_stok_barang["sku"],
+                            pinput_stok_barang["nama_barang"],
+                            pinput_stok_barang["harga_satuan"],
+                            pinput_stok_barang["jumlah_stok"],
+                        )
+                        if bst.insert(barang):
+                            put_info("Barang berhasil ditambahkan.")
+        elif sbmn_stok_barang == "2. Restok Barang":
+            with use_scope("message", clear=True):
+                data = bst.print_bst()
+                pinput_restok_barang = popup_input(
+                    [
+                        put_input(
+                            "jmh_stok_baru",
+                            label="Masukkan No.sku barang yang akan di restok : ",
+                            type=NUMBER,
+                        ),
+                        put_table(
+                            data,
+                            header=[
+                                "No. SKU",
+                                "Nama Barang",
+                                "Harga Satuan",
+                                "Jumlah Stok",
+                            ],
+                        ),
+                    ],
+                    title="Restok barang",
+                )
+                no_sku_barang = bst.find_node(pinput_restok_barang["jmh_stok_baru"])
+                if bst.contains(pinput_restok_barang["jmh_stok_baru"]) is None:
+                    put_info(f"Barang dengan No.sku {no_sku_barang} tidak ditemukan.")
+                else:
+                    stok_baru = popup_input(
+                        [
+                            put_input(
+                                "stok_baru",
+                                label="Masukkan jumlah stok tambahan",
+                                type=NUMBER,
+                            )
+                        ]
+                    )
+                    no_sku_barang.jumlah_stok += stok_baru["stok_baru"]
+                    put_info(
+                        f"Stok barang dengan No.sku {no_sku_barang}  berhasil ditambahkan\njumlah stok sekarang : {no_sku_barang.jumlah_stok}"
+                    )
         elif sbmn_stok_barang == "3. Kembali ke menu utama":
             main_sistem()
             break
@@ -114,8 +184,6 @@ def stok_barang():
 def main_sistem():
     clear()
     while True:
-        # put_text("SITORASI")
-        # put_text("Sistem Informasi Stok dan Transaksi")
         put_markdown("### SITORSI")
         put_markdown("### Sistem Informasi Stok dan Transaksi")
         mn_stok_barang = radio(
